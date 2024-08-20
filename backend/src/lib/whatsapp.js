bc2f2daa2ea9ac9b30478d548302491d09378051
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-08-20 02:42:06 -0300

    qr

--- backend/src/lib/whatsapp.js
+++ backend/src/lib/whatsapp.js
@@ -1,24 +1,28 @@
-const qrcode = require('qrcode'); // Cambiamos a 'qrcode' para poder generar la imagen del QR
-const { Client, LocalAuth } = require('whatsapp-web.js');
-
-let qrCodeData = '';  // Variable para almacenar el QR Code
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth  } = require('whatsapp-web.js');
 
 const whatsapp = new Client({
   puppeteer: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  },
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	},
   authStrategy: new LocalAuth(),
+//   webVersion: '2.2412.54',
+  // webVersionCache: {
+  //   type: 'remote',
+  //   remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+  // },
 });
 
 whatsapp.on('qr', qr => {
-  qrCodeData = qr;  // Almacenamos el código QR recibido
-  console.log('QR Code received.');
+  qrCodeData = qr;
+  qrcode.generate(qr, {
+      small: true
+      
+  });
 });
 
 whatsapp.on('ready', () => {
   console.log('Client is ready!');
 });
 
-whatsapp.initialize();
-
-module.exports = { whatsapp, qrCodeData };
+module.exports = {whatsapp,  qrCodeData};
