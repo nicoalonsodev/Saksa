6d8d8cba26b082cb16fddd7dc25e223ac0cd55d4
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-08-20 05:31:55 -0300

    codigo change

--- backend/src/handlers/postVerificationHandler.js
+++ backend/src/handlers/postVerificationHandler.js
@@ -7,7 +7,7 @@ const sendWhatsapp = async(req, res)=>{
   const chatId = phoneNumber.substring(1) + "@c.us";
   const number_details = await whatsapp.getNumberId(chatId);
   if(number_details){
-    const mensaje = `Este es tu codigo ${verificationCode}!`
+    const mensaje = `Este es tu codigo: ${verificationCode}`
     await whatsapp.sendMessage(chatId, mensaje);
     res.json({res: true})
   }else{
