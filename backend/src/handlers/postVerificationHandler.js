090e95f8cbf2f9a0001205970032135c729f19ff
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-08-20 03:27:40 -0300

    backend

--- backend/src/handlers/postVerificationHandler.js
+++ backend/src/handlers/postVerificationHandler.js
@@ -2,9 +2,9 @@ const {whatsapp} = require('../lib/whatsapp');
 
 
 const sendWhatsapp = async(req, res)=>{
-    const {verificationCode} = req.body;
+    const {verificationCode, phoneNumber} = req.body;
   const tel = '+5493815522572'
-  const chatId = tel.substring(1) + "@c.us";
+  const chatId = phoneNumber.substring(1) + "@c.us";
   const number_details = await whatsapp.getNumberId(chatId);
   if(number_details){
     const mensaje = `Este es tu codigo ${verificationCode}!`
