d93515af79293d90139e73a782b44f01f8669b58
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-08-20 02:11:42 -0300

    qr

--- backend/src/lib/whatsapp.js
+++ backend/src/lib/whatsapp.js
@@ -1,20 +1,21 @@
-const qrcode = require('qrcode');
+const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth  } = require('whatsapp-web.js');
 
 const whatsapp = new Client({
   puppeteer: {
 		args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  },
+	},
   authStrategy: new LocalAuth(),
+//   webVersion: '2.2412.54',
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+  },
 });
 
 whatsapp.on('qr', qr => {
-  qrcode.toFile('qrcode.png', qr, (err) => {
-    if (err) {
-      console.error('Failed to generate QR Code', err);
-    } else {
-      console.log('QR Code saved as qrcode.png');
-    }
+  qrcode.generate(qr, {
+      small: true
   });
 });
 
@@ -22,4 +23,4 @@ whatsapp.on('ready', () => {
   console.log('Client is ready!');
 });
 
-module.exports = { whatsapp };
+module.exports = {whatsapp};
